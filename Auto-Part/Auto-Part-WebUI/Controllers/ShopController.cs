eeef8c9afa3c4b5349c595ad99ff4b262ded7721
sahibtt <alisal@code.edu.az>
2022-07-02 15:07:33 +0400

    productUIDetails

--- Auto-Part/Auto-Part-WebUI/Controllers/ShopController.cs
+++ Auto-Part/Auto-Part-WebUI/Controllers/ShopController.cs
@@ -27,6 +27,24 @@ namespace Auto_Part_WebUI.Controllers
                 .ToList();
             return View(model);
         }
+        [Authorize(Policy = "shop.details")]
+        public IActionResult Details(int id)
+        {
+            var product = db.Products
+                .Include(p => p.Pricings)
+                .ThenInclude(p=>p.ProductType)
+                .Include(p=>p.Category)
+                .ThenInclude(p=>p.Brand)
+                .FirstOrDefault(p => p.DeletedById == null && p.Id==id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Pricings = db.ProductPricings
+                .Include(p=>p.ProductType)
+                .Where(p => p.ProductId == id);
+            return View(product);
+        }
         [Authorize(Policy = "shop.categories")]
         public IActionResult Categories(int id)
         {
