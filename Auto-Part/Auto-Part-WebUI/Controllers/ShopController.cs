df6e9129bb0a17ad9ad42ce19bcef510896a97ae
sahibtt <alisal@code.edu.az>
2022-07-06 08:57:34 +0400

    basket

--- Auto-Part/Auto-Part-WebUI/Controllers/ShopController.cs
+++ Auto-Part/Auto-Part-WebUI/Controllers/ShopController.cs
@@ -108,11 +108,10 @@ namespace Auto_Part_WebUI.Controllers
                 int[] idsFromCookie = cards.Split(",").Where(CheckIsNumber)
                         .Select(item => int.Parse(item))
                         .ToArray();
-                
-                var products = from p in db.Products.Where(p=>p.DeletedById==null)
+
+                var products = from p in db.Products.Where(p => p.DeletedById == null)
                                where idsFromCookie.Contains(p.Id) && p.DeletedById == null
                                select p;
-
                 return View(products.ToList());
                                
             }
