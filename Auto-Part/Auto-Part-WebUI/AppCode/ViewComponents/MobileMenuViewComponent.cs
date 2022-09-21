f8208f2395220d2d0468fcb4e2bde93a9ff39f44
sahibtt <alisal@code.edu.az>
2022-09-21 19:29:21 +0400

    something

---
+++ Auto-Part/Auto-Part-WebUI/AppCode/ViewComponents/MobileMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Auto_Part_WebUI.Models.DataContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auto_Part_WebUI.AppCode.ViewComponents
+{
+    public class MobileMenuViewComponent : ViewComponent
+    {
+        private readonly ECoPartDbContext db;
+
+        public MobileMenuViewComponent(ECoPartDbContext db)
+        {
+            this.db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.PopularCars = db.PopularCars
+                .Include(pc => pc.Brand)
+                .Where(c => c.DeletedById == null)
+                .ToList();
+
+
+            return View();
+        }
+    }
+}
